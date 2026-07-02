847e73fa19d960d82ecd87ae76dc141b66209049
Jeremy <jeremymoebel@gmail.com>
2026-07-01 23:47:33 -0400

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            real_line = [x for x in board[i] if x != "."]
+            if len(real_line) != len(set(real_line)):
+                return False
+        
+        for j in range(9):
+            seen = dict()
+            for i in range(9):
+                if board[i][j] == ".":
+                    continue
+                if board[i][j] in seen:
+                    return False
+                else:
+                    seen[board[i][j]] = True
+        
+        for i in range(0, 7, 3):
+            for j in range(0, 7, 3):
+                seen = dict()
+                for k in range(i, i+3):
+                    for l in range(j, j+3):
+                        if board[k][l] == ".":
+                            continue
+                        if board[k][l] in seen:
+                            return False
+                        else:
+                            seen[board[k][l]] = True
+
+        return True
+
+                
+                
+        
