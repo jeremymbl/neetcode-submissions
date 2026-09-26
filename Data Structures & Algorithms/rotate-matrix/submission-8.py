daa7d7a18676c0e4a0f251c1296d3c281ee30fbb
Jeremy <jeremymoebel@gmail.com>
2026-09-26 18:33:41 +0200

    Add: rotate-matrix - submission-8

---
+++ Data Structures & Algorithms/rotate-matrix/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        # on transpose
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # puis on fait la symetrie
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+        return
